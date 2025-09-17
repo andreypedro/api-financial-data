4b9e46064d5c5c891709df1737e14a085d0ef2d7
Andrey Lefkum <andreypedro@gmail.com>
2025-09-17 13:41:46 -0400

    feat: preparing for production

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -1,22 +1,26 @@
-import { NextFunction, Request, Response } from "express";
-import jwt from 'jsonwebtoken'
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 
 export const AuthenticationController = (req: Request, res: Response, next: NextFunction) => {
-    
-    const { username, password } = req.body
-    const JWT_SECRET = 'SECRET'
+   const { username, password } = req.body;
+   const JWT_SECRET = process.env.JWT_SECRET;
 
-    if(true) {
+   if (!JWT_SECRET) {
+      return res.status(500).json({
+         message: 'JWT_SECRET is not defined',
+      });
+   }
 
-        const user = {
-            username
-        }
+   if (true) {
+      const user = {
+         username,
+      };
 
-        const tokjen = jwt.sign(user, JWT_SECRET)
+      const tokjen = jwt.sign(user, JWT_SECRET);
 
-        res.json({
-            message: 'Login was succefuly retrieved',
-            tokjen
-        })
-    }
-}
+      res.json({
+         message: 'Login was succefuly retrieved',
+         tokjen,
+      });
+   }
+};
